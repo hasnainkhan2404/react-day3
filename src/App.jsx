1916f1343f3ae12baee80dfd70c5b4fb8121ed1d
HASNAIN KHAN <ak8876575@gmail.com>
2025-03-01 09:56:28 +0530

    counter-app

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,16 @@ function App() {
 
   return (
    <>
-   <div>
-    
+   <div className='border-4 border-brown p-8 rounded-lg'>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-2xl font-bold text-fuchsia-950 mb-8'>Counter App</h1>
+      <h2 className='text-4xl font-bold text-fuchsia-950 mb-8'>{count}</h2>
+      <div className='flex gap-8'>
+        <button className='border p-2 border-black' onClick={()=>setCount(count+1)}>Increment</button>
+        <button className='border p-2 border-black' onClick={()=>setCount(count-1)}>Decrement</button>
+        <button className='border p-2 border-black' onClick={()=>setCount(0)}>Reset</button>
+      </div>
+    </div>
    </div>
    </>
   )
